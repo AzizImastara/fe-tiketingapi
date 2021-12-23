c8dd748912d40578e32020f24c0f6de1c79b4ca8
Aziz Imastara <sazizimastara@gmail.com>
2021-12-23 13:01:32 +0700

    integrasi ticket

--- src/pages/main/Ticket/index.jsx
+++ src/pages/main/Ticket/index.jsx
@@ -4,102 +4,96 @@ import "./index.css";
 import Navs from "../../../components/Navbar";
 import Footer from "../../../components/Footer";
 import logo from "../../../assets/img/ticketwhite.png";
+import axios from "../../../utils/axios";
+import QRCode from "react-qr-code";
+import moment from "moment";
 
-const Ticket = () => {
-  let history = useHistory();
+const Ticket = (props) => {
+  const [dataTicket, setDataTicket] = useState({});
+  const [dataBooking, setDataBooking] = useState({});
+
+  console.log(dataBooking, "data");
+  const getBookingById = () => {
+    axios
+      .get(`booking/get_booking/${props.match.params.id}`)
+      .then((res) => {
+        setDataBooking(res.data.data[0]);
+        // console.log(res, "booking");
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+
+  useEffect(() => {
+    console.log(props.match.params.id, "tikec");
+    getBookingById();
+  }, []);
 
   return (
     <>
       <Navs />
       <div className="ticket__bg">
         <div className="container">
-          <div className="ticket__header">
-            <h2 className="text-center">Proff of Payment</h2>
-          </div>
-          <div className="row">
-            <div className="col-2"></div>
-            <div className="col-5">
-              <div className="ticket__header--title">
-                <img src={logo} />
-                <span>Admit One</span>
-              </div>
-              <div className="ticket__desc">
-                <span>Movie</span>
-                <h6>Spiderman HomeComing</h6>
-              </div>
-              <div className="row ticket__desc--schedule">
-                <div className="col-4">
-                  <div className="ticket__desc--schedule--item">
-                    <span>Date</span>
-                    <h6>07 July</h6>
+          <div className="ticket__background">
+            <div className="ticket__header">
+              <h2>Proff of Payment</h2>
+            </div>
+            <div className="row ticket__content">
+              <div className="col-lg-7 ticket__content--1">
+                <div className="ticket__header--title">
+                  <img src={logo} />
+                  <span>Admit One</span>
+                </div>
+                <div className="ticket__desc">
+                  <span>Movie</span>
+                  <h6>{dataBooking.name}</h6>
+                </div>
+                <div className="row ticket__desc--schedule">
+                  <div className="col-4">
+                    <div className="ticket__desc--schedule--item">
+                      <span>Date</span>
+                      <h6>{moment(dataBooking.dateBooking).format("dddd MMM YYYY")}</h6>
+                    </div>
+                    <div className="ticket__desc--schedule--item">
+                      <span>Count</span>
+                      <h6>{dataBooking.totalTicket} pieces</h6>
+                    </div>
                   </div>
-                  <div className="ticket__desc--schedule--item">
-                    <span>Count</span>
-                    <h6>3 pieces</h6>
+                  <div className="col-4">
+                    <div className="ticket__desc--schedule--item">
+                      <span>Time</span>
+                      <h6>{dataBooking.timeBooking}</h6>
+                    </div>
+                    <div className="ticket__desc--schedule--item">
+                      <span>Seats</span>
+                      <h6>{(dataBooking.seat || []).map((item) => `${item}, `)}</h6>
+                      {/* <h6>{dataBooking.seat.join(", ")}</h6> */}
+                    </div>
+                  </div>
+                  <div className="col-4">
+                    <div className="ticket__desc--schedule--item">
+                      <span>Price</span>
+                      <h6>Rp.{dataBooking.totalPayment}</h6>
+                    </div>
                   </div>
                 </div>
-                <div className="col-4">
-                  <div className="ticket__desc--schedule--item">
-                    <span>Time</span>
-                    <h6>02:00 am</h6>
-                  </div>
-                  <div className="ticket__desc--schedule--item">
-                    <span>Seats</span>
-                    <h6>C4, C5, C6</h6>
-                  </div>
+              </div>
+              <div className="col-lg-5 ticket__content--2">
+                <div className="ticket__header--title--right">
+                  <img src={logo} />
                 </div>
-                <div className="col-4">
-                  <div className="ticket__desc--schedule--item">
-                    <span>Category</span>
-                    <h6>PG-13</h6>
-                  </div>
-                  <div className="ticket__desc--schedule--item">
-                    <span>Price</span>
-                    <h6>$30,00</h6>
-                  </div>
+                <div className="qr">
+                  <QRCode
+                    value={`${process.env.REACT_APP_URL_LOCAL}booking/qr/${props.match.params.id}`}
+                  />
                 </div>
               </div>
             </div>
-            <div className="col-3">
-              <div className="ticket__header--title--right">
-                <img src={logo} />
-              </div>
-              <div className="ticket__desc">
-                <span>Movie</span>
-                <h6>Spiderman</h6>
-              </div>
-              <div className="row ticket__desc--schedule">
-                <div className="col-5">
-                  <div className="ticket__desc--schedule--item">
-                    <span>Date</span>
-                    <h6>07 Jul</h6>
-                  </div>
-                  <div className="ticket__desc--schedule--item">
-                    <span>Count</span>
-                    <h6>3 pcs</h6>
-                  </div>
-                </div>
-                <div className="col-7">
-                  <div className="ticket__desc--schedule--item">
-                    <span>Time</span>
-                    <h6>02:00 am</h6>
-                  </div>
-                  <div className="ticket__desc--schedule--item">
-                    <span>Seats</span>
-                    <h6>C6, C7, C8</h6>
-                  </div>
-                  <div className="ticket__desc--schedule--item">
-                    <span>Category</span>
-                    <h6>PG-13</h6>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-2"></div>
-          </div>
-          <div className="ticket__button">
-            <button>Download</button>
-            <button>Print</button>
+            {/* <div className="ticket__button">
+              <button>Download</button>
+              <button>Print</button>
+            </div> */}
           </div>
         </div>
       </div>
